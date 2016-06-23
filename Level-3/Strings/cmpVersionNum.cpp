74852bb70a9deb0e78b57768ed0870f834cb4319
Siddharth Gupta <siddharthgupta234@gmail.com>
2016-06-23 08:47:34 +0530

    Create cmpVersionNum.cpp

---
+++ Level-3/Strings/cmpVersionNum.cpp
@@ -0,0 +1,50 @@
+// https://www.interviewbit.com/problems/compare-version-numbers/?ref=similar_problems
+
+int Solution::compareVersion(string A, string B) {
+    int i = 0, j = 0;
+    
+    while(i<A.length() && j<A.length()){
+        string x="";
+        string y="";
+        
+        while(i<A.length() && A[i]=='0'){
+                i++;
+        }
+            
+        while(i<A.length() && A[i]!='.'){
+            x+=A[i];
+            i++;
+        }
+        
+        while(j<B.length() && B[j]=='0'){
+                j++;
+        }
+            
+        while(j<B.length() && B[j]!='.'){
+            y+=B[j];
+            j++;
+        }
+        
+        if(x!=y){
+            if(x.length()==y.length())
+                return x.compare(y) > 0 ? 1: -1;
+            return x.length()>y.length() ? 1:-1;
+        }
+        
+        i++;
+        j++;
+    }
+    
+    while(i<A.length() && A[i]=='0'){
+        i++;
+    }
+    
+    while(j<B.length() && B[j]=='0'){
+        j++;
+    }
+    
+    if(i>=A.length() && j>=B.length())
+        return 0;
+    
+    return i>j ? 1 : -1;
+}

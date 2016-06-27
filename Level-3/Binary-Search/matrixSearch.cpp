83a4db1b40f2e6bf0f7adeaa12b4ab9310918554
Siddharth Gupta <siddharthgupta234@gmail.com>
2016-06-27 11:18:46 +0530

    Update matrixSearch.cpp

--- Level-3/Binary-Search/matrixSearch.cpp
+++ Level-3/Binary-Search/matrixSearch.cpp
@@ -4,17 +4,9 @@ int Solution::searchMatrix(vector<vector<int> > &A, int B) {
     int numOfRows = A.size();
     int numOfColumns = A[0].size();
     int low = 0, high = numOfRows*numOfColumns-1;
-    int ans =0;
     
     while(low<=high){
-        int lowX = low / numOfColumns;
-        int lowY = low % numOfColumns;
-        
-        int highX = high / numOfColumns;
-        int highY = high % numOfColumns;
-        
         int mid = (low+high)/2;
-        
         int midX = mid / numOfColumns;
         int midY = mid % numOfColumns;
         
@@ -28,7 +20,6 @@ int Solution::searchMatrix(vector<vector<int> > &A, int B) {
         else{
             low = mid+1;
         }
-    
     }
     
     return 0;

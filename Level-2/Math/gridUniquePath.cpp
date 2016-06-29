8c87282f1826aa12edae29c7e32b8b4deeaf71a8
Siddharth Gupta <siddharthgupta234@gmail.com>
2016-06-29 10:36:00 +0530

    Create gridUniquePath.cpp

---
+++ Level-2/Math/gridUniquePath.cpp
@@ -0,0 +1,12 @@
+// https://www.interviewbit.com/problems/grid-unique-paths/
+
+int Solution::uniquePaths(int A, int B) {
+    int ans = 1;
+    int x = min (B, A); // if we dont use the min value, ans may overflow.
+    
+    for(int i=1;i<x;i++){
+        ans = (ans *( A+B-2 - i +1))/i;
+        
+    }
+    return ans;
+}

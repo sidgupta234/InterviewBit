61e6b23bc8918b9f01284e39e88d33bf13de7bcc
Siddharth Gupta <siddharthgupta234@gmail.com>
2016-07-10 17:54:49 +0530

    Create minStack.cpp

---
+++ Level-4/Stacks-And-Queues/minStack.cpp
@@ -0,0 +1,36 @@
+stack <int> minAns;
+stack <int> values; 
+
+MinStack::MinStack() {
+    while(!minAns.empty())
+        minAns.pop();
+        
+    while(!values.empty())
+        values.pop();
+}
+
+void MinStack::push(int x) {
+    if( minAns.empty() || x<=minAns.top() )
+        minAns.push(x);
+    values.push(x);
+}
+
+void MinStack::pop() {
+    if(values.empty())
+        return;
+        
+    if ( values.top() == minAns.top() ){
+            values.pop();
+            minAns.pop();
+        }
+    else
+        values.pop();
+}
+
+int MinStack::top() {
+    return values.empty() ? -1 : values.top();
+}
+
+int MinStack::getMin() {
+    return minAns.empty() ? -1 : minAns.top();
+}

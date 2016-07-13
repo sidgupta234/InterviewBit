1b96f74290c7420e4bc4000111aff3c1a7737386
Siddharth Gupta <siddharthgupta234@gmail.com>
2016-07-13 19:14:40 +0530

    Update pointsOnLine2.cpp

--- Level-5/Hashing/pointsOnLine2.cpp
+++ Level-5/Hashing/pointsOnLine2.cpp
@@ -11,35 +11,33 @@ int Solution::maxPoints(vector<int> &A, vector<int> &B) {
 
         map <double, int> M;
         
-        for(int j=0;j<A.size();j++){
-
-            if(i!=j){
-                double x = A[j] - A[i];
-                double y = B[j] - B[i];
+        for(int j=i+1;j<A.size();j++){
+            double x = A[j] - A[i];
+            double y = B[j] - B[i];
+            
+            if(x==0){
                 
-                if((int)x==0){
-                    
-                    if((int)y==0){
-                        dup++;
-                    }
-                    
-                    else{
-                        vert++;
-                    }
+                if(y==0){
+                    dup++;
                 }
                 
                 else{
-                    M[y/x]+=1;
+                    vert++;
                 }
             }
+            
+            else{
+                M[y/x]+=1;
+            }
+        
         }
-        int pp =0;
+        
         
         for(map <double, int>::iterator it=M.begin() ;it!=M.end(); it++){
             result=max(result,it->second+dup);
         }
         
-        result = max(result, dup+vert); // as vert can be considered as another slope value.
+        result = max(result, dup+vert); // as vert can be considered as another slope values.
 
     }
     return result;

481bde0188ace891d25e3ec3f02f837b05d361d2
Siddharth Gupta <siddharthgupta234@gmail.com>
2016-08-27 17:28:58 +0530

    Create canCompleteCircuit.cpp

---
+++ Level-7/Greedy/canCompleteCircuit.cpp
@@ -0,0 +1,25 @@
+// https://www.interviewbit.com/problems/gas-station/
+// http://stackoverflow.com/a/37391395/3578017
+int Solution::canCompleteCircuit(const vector<int> &gas, const vector<int> &cost) {
+    int ansind = 0;
+    int net = 0;
+    int total = 0;
+    
+    for(int i=0; i<gas.size(); i++){
+        net += gas[i]-cost[i];
+        
+        if(net<0){
+            ansind = i+1;
+            net = 0;
+        }
+        
+        total += gas[i]-cost[i];
+    }
+    
+    if(total>=0)
+        return ansind;
+                
+    return -1;
+    
+    return ansind;
+}

ed22690f721edb7111fd5ef6ad8aa4b56ef72970
Siddharth Gupta <siddharthgupta234@gmail.com>
2016-10-27 09:56:09 +0530

    Update maxSumKadane.cpp

--- Level-2/Arrays/maxSumKadane.cpp
+++ Level-2/Arrays/maxSumKadane.cpp
@@ -1,24 +1,19 @@
 // https://www.interviewbit.com/problems/max-sum-contiguous-subarray/
-int midMax(vector <int>A, int start, int mid, int end);
-int recurseAns(vector<int> A, int start, int end);
 
 int Solution::maxSubArray(const vector<int> &A) {
-    int maxTillNow=0, cumulative=0, minNeg=INT_MIN;
+    int ans = INT_MIN;
+    int cumulative = 0;
+    int maxval = INT_MIN;
     
-    for(int i=0; i< A.size(); i++){
-        cumulative +=A[i];
+    for(int i=0; i<A.size(); i++){
+        maxval = max(maxval, A[i]);
+        cumulative += A[i];
+        ans = max(cumulative, ans);
         
-        if(A[i] <= 0){
-            minNeg = max(minNeg, A[i]);
-        }
-
-        if(cumulative < 0)
+        if(cumulative<0){
             cumulative =0;
-        
-        if(cumulative > maxTillNow)
-            maxTillNow = cumulative;
-            
+        }
     }
     
-    return maxTillNow==0 ? minNeg : maxTillNow;
+    return max(ans, maxval);
 }
